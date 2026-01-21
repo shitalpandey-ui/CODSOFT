ba8d45860c48ff237e429e738d3689c7c2451b9a
shitalpandey <shitalpandey940@gmail.com>
2026-01-21 20:47:44 +0545

    Update calculator.js

--- task 3/calculator.js
+++ task 3/calculator.js
@@ -1,11 +1,14 @@
  
  let currentvalue = '';
         let previousvalue = '';
-        let operator = '';
+        let operator = null;
 
       function updateDisplay(){
         document.getElementById('currentvalue').textContent = currentvalue;
-          if(operator){
+          if(operator){ 
+
+
+
             document.getElementById('previousvalue').textContent= `${previousvalue} ${operator}`;
           }
           else{
@@ -34,37 +37,38 @@
            updateDisplay();
         }
      function calculate() {
-            if (currentvalue==='' || previousvalue==='' || operator === null) return;
+    if (currentvalue === '' || previousvalue === '' || operator === null) return;
 
-            let result=0;
-            const prev = parseFloat(previousvalue);
-            const current = parseFloat(currentvalue);
+    const prev = parseFloat(previousvalue);
+    const current = parseFloat(currentvalue);
 
-            if (isNaN(prev) || isNaN(current)) 
+    if (isNaN(prev) || isNaN(current)) return;
 
-              switch (operator) {
-                case '+':
-                    result = prev + current;
-                    break;
-                case '-':
-                    result = prev - current;
-                    break;
-                case '*':
-                    result = prev * current;
-                    break;
-                
-            }
+    let result;
 
-            currentvalue = console.log(result);
-            operator = '';
-            previousvalue = '';
-           updateDisplay();
-        }
+    switch (operator) {
+        case '+':
+            result = prev + current;
+            break;
+        case '-':
+            result = prev - current;
+            break;
+        case '*':
+            result = prev * current;
+            break;
+    }
+
+    currentvalue = result.toString();
+    operator = null;
+    previousvalue = '';
+    updateDisplay();
+}
+
     
          function clearDisplay() {
             currentvalue = '';
             previousvalue = '';
-            operator = '';
+            operator = null;
            updateDisplay();
         }
       
